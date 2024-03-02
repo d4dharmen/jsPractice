8fdf757e6493a816436d11bb6772e4f8ea2d5b1f
d4dharmen <d4dharmen@gmail.com>
2024-03-02 12:31:37 +0530

    2_progressBar project wip, circle highlight on next previous button click working, only line highlight left to work

--- 2_progressBar/script.js
+++ 2_progressBar/script.js
@@ -1,1 +1,45 @@
-console.log("hellow from js");
+
+const numbers = document.querySelectorAll(".num")
+const nextButton = document.getElementById("nxt")
+const previousButton = document.getElementById("prev")
+let activeNumber = 0;
+// console.log("onload activenumber " + activeNumber);
+
+//on load condition
+numbers[activeNumber].classList.add("active")
+previousButton.disabled = "disabled"
+
+function handleNext() {
+    if (activeNumber < 2) {
+        previousButton.disabled = "";
+        activeNumber = activeNumber+1;
+        numbers[activeNumber].classList.add("active")
+        previousButton.classList.add("active")
+        // console.log("next if activeNumber "+activeNumber);
+        
+    } else {
+        activeNumber = 3;
+        numbers[activeNumber].classList.add("active")
+        nextButton.disabled = "disabled";
+        nextButton.classList.remove("active")
+        // console.log("next else activeNumber "+activeNumber);
+    }
+    
+}
+function handlePrevious() {
+    numbers[activeNumber].classList.remove("active")
+    activeNumber = activeNumber - 1;
+    if (activeNumber <= 0) {
+        previousButton.classList.remove("active")
+        previousButton.disabled = "disabled"
+        activeNumber = 0;
+        nextButton.classList.add("active")
+        nextButton.disabled = "";
+        // console.log("previous if activeNumber "+activeNumber);
+    } else {
+        // console.log("previous else activeNumber "+activeNumber);
+    }
+}
+
+nextButton.addEventListener("click", handleNext)
+previousButton.addEventListener("click", handlePrevious)
